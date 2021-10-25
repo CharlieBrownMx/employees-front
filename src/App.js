dd0735f1cbf80cc86c0c627bb63793da7ada5b7f
Esteban Cruz Torres <carlos.cruz19@outlook.fr>
2021-10-25 03:49:19 -0500

    First structure for the frontend service

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Employees from './components/Employee';
+import Pagination from './components/Pagination';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [employeesPerPage] = useState(40);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('http://localhost:3000/employeesExample');
+      setEmployees(res.data.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current employees
+  const indexOfLastEmployee = currentPage * employeesPerPage;
+  const indexOfFirstEmployee = indexOfLastEmployee - employeesPerPage;
+  const currentEmployees = employees.slice(indexOfFirstEmployee, indexOfLastEmployee);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
+  return (
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>Javascript Fullstack - Code Challenge <img src={logo} className="App-logo" alt="logo" /></h1>
+      <Employees employees={currentEmployees} loading={loading} />
+      <Pagination
+        employeesPerPage={employeesPerPage}
+        totalEmployees={employees.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+};
+
+export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Employee.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Employees = ({ employees, loading }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <table className="table ">
+      <thead>
+        <tr>
+          <td>emp_no</td>
+          <td>birth_date</td>
+          <td>first_name</td>
+          <td>last_name</td>
+          <td>gender</td>
+          <td>hire_date</td>
+        </tr>
+      </thead>
+      <tbody>
+        {employees.map(employee => (
+          <tr key={employee.emp_no}>
+            <td>{employee.emp_no}</td>
+            <td>{ employee.birth_date }</td>
+            <td>{ employee.first_name }</td>
+            <td>{ employee.last_name }</td>
+            <td>{ employee.gender }</td>
+            <td>{ employee.hire_date }</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Employees;
